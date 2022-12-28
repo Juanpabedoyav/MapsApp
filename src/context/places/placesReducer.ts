69fa90646af33521e32a4554ba3417e8bf31b164
Juan Pa Bedoya <bedoyavascojuanpablo@gmail.com>
2022-12-27 19:54:22 -0500

    add: set user location action for manage the state

--- src/context/places/placesReducer.ts
+++ src/context/places/placesReducer.ts
@@ -1,6 +1,21 @@
+import { PlacesState } from "./PlacesProvider";
 
+type PlaceAction = { type: 'setUserLocation', payload:[number, number] }
 
-export const placesReducer = () =>{
+export const placesReducer = ( state:PlacesState, action:PlaceAction ):PlacesState =>{
+    switch (action.type) {
+        case 'setUserLocation':
+            return{
+                ...state,
+                isLoading: false,
+                userLocation: action.payload
+            }
+    
+        default:
+            return{
+                ...state,
+            }
+    }
 
     
 }
